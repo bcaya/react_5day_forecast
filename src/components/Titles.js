478ca90618c19ee52fce33e11798e92883fb74de
Bobby Caya <bob.caya1@gmail.com>
2018-06-26 09:57:03 -0600

    stub out components

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Titles extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>5 Day Weather Finder</h1>
+        <p>Learn what the weather is for the next 5 days!</p>
+      </div>
+    )
+  }
+}
+
+export default Titles;
