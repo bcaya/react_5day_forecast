6b98791221689dc6c99e3431ada40c5846b465d7
Bobby Caya <bob.caya1@gmail.com>
2018-07-03 18:38:19 -0600

    figured out how to parse and sort json data based on date

--- src/App.js
+++ src/App.js
@@ -8,9 +8,33 @@ const API_KEY = `${process.env.REACT_APP_API_KEY}`;
 class App extends React.Component { 
   getForecast = async (e) => {
     e.preventDefault();
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Manchester,uk&appid=${API_KEY}&units=imperial`);
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const tempArrays = []
+    const newArray = []
+    const temp = undefined
+    const temp_max = undefined
+    const temp_min = undefined
+    const humidity = undefined
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=${API_KEY}&units=imperial`);
     const data = await api_call.json();
     console.log(data);
+    const listArray=data.list
+    console.log(listArray)
+    let newArr = listArray.map(day => {
+      const date = new Date(day.dt * 1000);
+      const days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+      const name = days[date.getDay()];
+      const temp = day.main.temp;
+      const temp_max = day.main.temp_max;
+      const temp_min = day.main.temp_min;
+      const humidity = day.main.humidity;
+      const description = day.weather[0].description;
+      console.log(`date: ${date} name: ${name} temp: ${temp} temp_max: ${temp_max} temp_min: ${temp_min} humidity: ${humidity} description: ${description}`);
+      return newArr = [{date}, {name}, {temp}, {temp_max}, {temp_min}, {humidity}, {description}];
+    })
+    console.log(JSON.stringify(newArr))
+    // console.log(`tempArrays: ${newArr} newArrays: ${newArray}`)
   }
   render(){
     return(
